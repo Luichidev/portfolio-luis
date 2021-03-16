8bf5cb7d0c4706a253e4eff3ea17bdf6a600bf1f
@LuisArana <disconluis@gmail.com>
2021-03-16 20:32:28 +0100

    Se creo el proyecto nuevo y se creo los componentes vacios

---
+++ components/Header/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Header() {
+  return (
+    <>
+      <Head></Head>
+      <header></header>
+      <style jsx>{``}</style>
+    </>
+  );
+}
---
+++ components/Navbar/index.js
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <>
+      <nav></nav>
+      <style jsx>{``}</style>
+    </>
+  );
+}
