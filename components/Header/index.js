bd73a926c5788ea8ce310c1ca2697fbd0e499ee1
@LuisArana <disconluis@gmail.com>
2021-03-21 11:19:55 +0100

    Se agrego los metas y se creo estructura del home page

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,8 +3,56 @@ import Head from "next/head";
 export default function Header() {
   return (
     <>
-      <Head></Head>
-      <header></header>
+      <Head>
+        <title>Luis Alberto | Portfolio WEB</title>
+        <meta property="og:site_name" content="Portafolio WEB Luis" />
+        <meta property="og:site_type" content="website" />
+        <meta property="og:title" content="Portfolio WEB" />
+        <meta property="og:url" content="https://luisalbertoarana.com" />
+        <meta property="og:image" content="assets/img/og-image.jpg" />
+        <meta
+          property="og:description"
+          content="Hola, Soy Luis Alberto, Bienvenid@s a mi portafolio WEB, aquí encontrarás información acerca de mi y mis proyectos; ¡Te invito a verla! :D"
+        />
+        <meta
+          name="keywords"
+          content="portfolio, portfolio web, portfolio WEB, Portfolio, Portfolio WEB, curriculum, website, luis, luis alberto, Luis, Luis Alberto, book web"
+        />
+        <meta
+          name="description"
+          content="Hola, Soy Luis Alberto, Bienvedi@s a mi portafolio WEB, aqui encontrarás información acerca de mi y mis proyectos; ¡Te invito a verla! :D"
+        />
+        <meta name="theme-color" content="#4A86E8" />
+        <meta name="MobileOptimized" content="width" />
+        <meta name="HandheldFriendly" content="true" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="author" content="assets/humans.txt" />
+        <link rel="apple-touch-icon" href="assets/img/icon-32x32.png" />
+        <link
+          rel="apple-touch-startup-image"
+          href="assets/img/icon-32x32.png"
+        />
+        <link
+          rel="shortcut icon"
+          type="image/png"
+          href="assets/img/icon-32x32.png"
+        />
+        <link rel="author" type="text/plain" href="assets/humans.txt" />
+        <link
+          rel="sitemap"
+          type="application/xml"
+          title="Sitemap"
+          href="assets/sitemap.xml"
+        />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
+        <script src="https://www.google.com/recaptcha/api.js"></script>
+        <link rel="manifest" href="manifest.json" />
+        <meta name="theme-color" content="#1976d2" />
+      </Head>
+      <header>Header</header>
       <style jsx>{``}</style>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <Header></Header>
+      <Navbar></Navbar>
+      <main></main>
+      <Footer></Footer>
+    </div>
+  );
 }
