3106457c6e969002cefb005bb96385f3157b1de4
@LuisArana <disconluis@gmail.com>
2021-03-21 11:21:09 +0100

    Se implemento el navbar , se puso el favicon y el manifiest

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,7 +1,42 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function Navbar() {
+  const router = useRouter();
+
+  const handleOpenMenu = () => {
+    document.getElementById("btnMenu").classList.toggle("show");
+  };
+
   return (
     <>
-      <nav></nav>
+      <nav>
+        <span onClick={handleOpenMenu}>
+          {/* <Menu stroke="#00a551"></Menu> */}
+        </span>
+        <ul id="btnMenu">
+          <li className={router.pathname == "/" ? "active" : ""}>
+            <Link href="/">
+              <a>Inicio</a>
+            </Link>
+          </li>
+          <li className={router.pathname == "/formacion" ? "active" : ""}>
+            <Link href="/formacion">
+              <a>Formación</a>
+            </Link>
+          </li>
+          <li className={router.pathname == "/aptitudes" ? "active" : ""}>
+            <Link href="/aptitudes">
+              <a>Aptitudes</a>
+            </Link>
+          </li>
+          <li className={router.pathname == "/proyectos" ? "active" : ""}>
+            <Link href="/proyectos">
+              <a>Proyectos</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <style jsx>{``}</style>
     </>
   );
--- pages/contacto/index.js
+++
@@ -1,3 +0,0 @@
-export default function Contacto() {
-  return <h1>Contactos</h1>;
-}
