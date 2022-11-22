e5ef3760b24c85b5b7d20443b3a5970ec7f4b93c
Luis Alberto Arana <luichidev@gmail.com>
2022-11-22 23:12:34 +0100

    feat: Update professional training

--- components/Models/formacion.js
+++ components/Models/formacion.js
@@ -1,50 +1,38 @@
 const formacion = [
-  {
-    id: 1,
-    title: "CONOCIMIENTOS",
-    sub_title: "Tecnologías",
-    image: "/assets/img/conocimiento.jpg",
-    icon: "🛠️",
-    skill: [
-      "HTML5 / CSS3 / JavaScript / Bootstrap / Bootswatch / Font-Awesome",
-      "PHP / MySql / WordPress / NodeJS / Express / MongoDB / MariaDB",
-      "JQuery / JQueryUI / REACT / JSON",
-      "Gimp2 / Gravit Designer / MSOffice / Pencil",
-      "VirtualBox",
-      "Windows Server 2012RT / Ubuntu",
-    ],
-  },
-  {
-    id: 2,
-    title: "FORMACION",
-    sub_title: "Estudios Complementarios",
-    image: "/assets/img/formacion.jpg",
-    icon: "💻",
-    skill: [
-      "Ofimática (Word, Excel, PowerPoint, Access)",
-      "POO (Programación Orientada a Objetos) Lenguaje JAVA",
-      "Ingeniería de Aplicaciones Multimedia y juegos - Instituto ECC Barcelona",
-      "Castellano nativo / Catalán B2",
-      "Ingles Técnico Básico - UAGRM (Universidad Autónoma Gabriel Rene Moreno)",
-      "Técnico en prevención de riesgos laborales 30 hrs.",
-    ],
-  },
-  {
-    id: 3,
-    title: "EDUCACION",
-    sub_title: "Estudios Realizados",
-    image: "/assets/img/educacion.jpg",
-    icon: "🎓",
-    skill: [
-      "Bachillerato Colegio San Juan Bosco-1997",
-      "Ingeniería Informática UAGRM-2003",
-      "Escuela para Adultos Catalán nivel B2-2005-2008",
-      "Bachillerato Homologado Generalidad de Catalunya-2014",
-      "Tècnic en Sistemes microinformàtics i Xarxes - CEAC-2015",
-      "Confecció i publicació de pàgines web - EMFO Mollet-2018",
-      "Dessenvolupament D'Aplicaciones Amb Tecnologia Web - Cal Molins-2021",
-    ],
-  },
-];
+	{
+		id: 1,
+		title: 'CONOCIMIENTOS',
+		sub_title: 'Tecnologías',
+		image: '/assets/img/conocimiento.jpg',
+		icon: '🛠️',
+		skill: ['HTML5 / CSS3 / JavaScript / Bootstrap / Bootswatch / Font-Awesome', 'PHP / MySql / WordPress / NodeJS / Express / MongoDB / MariaDB', 'JQuery / JQueryUI / React / JSON / Vue', 'Gimp2 / Gravit Designer / MSOffice / Pencil', 'VirtualBox / VMWare', 'Windows Server 2012RT / Ubuntu / Kubuntu']
+	},
+	{
+		id: 2,
+		title: 'FORMACION',
+		sub_title: 'Estudios Complementarios',
+		image: '/assets/img/formacion.jpg',
+		icon: '💻',
+		skill: [
+			'Ofimática (Word, Excel, PowerPoint, Access)',
+			'POO (Programación Orientada a Objetos) Lenguaje JAVA',
+			'Ingeniería de Aplicaciones Multimedia y juegos - Instituto ECC Barcelona',
+			'Castellano nativo / Catalán B2',
+			'Ingles Técnico Básico - UAGRM (Universidad Autónoma Gabriel Rene Moreno)',
+			'Técnico en prevención de riesgos laborales 30 hrs.',
+			'Confecció i publicació de pàgines web - EMFO Mollet - 2018',
+			"Dessenvolupament D'Aplicaciones Amb Tecnologia Web - Cal Molins - 2021",
+			'Desarrolador FrontEnd - UOC - 2022'
+		]
+	},
+	{
+		id: 3,
+		title: 'EDUCACION',
+		sub_title: 'Estudios Realizados',
+		image: '/assets/img/educacion.jpg',
+		icon: '🎓',
+		skill: ['Bachillerato Colegio San Juan Bosco - 1997', 'Ingeniería Informática UAGRM - 2003', 'Escuela para Adultos Catalán nivel B2-2005-2008', 'Bachillerato Homologado Generalidad de Catalunya - 2014', 'Tècnic en Sistemes microinformàtics i Xarxes - CEAC - 2015']
+	}
+]
 
-export default formacion;
+export default formacion
